008431937524543464ee1533e56332bf76eb9153
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-10-02 20:25:44 -0400

    edits

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+//resource: video Jim provided in Slack: https://www.youtube.com/watch?v=SBwoFkRjZvE
+//resource: video Jim provided in Slack: https://www.youtube.com/watch?v=Nau-iEEgEoM
+
+//callbacks are a function that is evoked or called after something happens
+//lry logCall = function () {
+//   console.log("locCall was called back");
+// };
+// setTimeout(logCall, 3000);
+//event listeners like clicking on a button
+
+//wins and losses begin with 0
+let wins = 0
+let losses = 0
+
+
+//guesses begins with 9
+
+//guesses so far lists all the letters the person guesses with a comma in between each guess
+//once guesses counts down to 0 then guesses so far resets to nothing
+//if the person guesses the correct letter then wins go up by 1
+//if the person guesses 9 guesses without the correct letter then losses goes up by 1
+//need to define what the random letter is for each game
+
+
+
